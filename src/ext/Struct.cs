1108ba36edd4d9bc5b9c8a2683a9286b27c2f238
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-03-22 10:12:46 +0100

    struct to bytes

---
+++ src/ext/Struct.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SearchAThing.Ext;
+
+public static partial class Ext
+{
+
+    /// <summary>
+    /// retrieve a binary representation of given struct
+    /// </summary>
+    public static byte[] ToBytes<T>(this T obj) where T : struct
+    {
+        var size = Marshal.SizeOf(obj);
+        var arr = new byte[size];
+
+        var ptr = Marshal.AllocHGlobal(size);
+        Marshal.StructureToPtr(obj, ptr, true);
+        Marshal.Copy(ptr, arr, 0, size);
+        Marshal.FreeHGlobal(ptr);
+
+        return arr;
+    }
+
+}
